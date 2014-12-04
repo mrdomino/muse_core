c76c27827bcb997d14a62d216d1bf999e5670db8
Steven Dee <steve@smartercode.net>
2014-12-03 22:26:43 -0500

    Remove unused includes

--- include/all.h
+++ include/all.h
@@ -3,12 +3,10 @@
 #include <ctype.h>
 #include <errno.h>
 #include <limits.h>
-#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 
 #include "util.h"
 #include "version.h"
