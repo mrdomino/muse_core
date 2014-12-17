be98b8aa25b4dbe1408ed41069a974ee060a25be
Steven Dee <steve@smartercode.net>
2014-12-17 00:34:03 -0500

    Batteries included header

--- src/muse_core.h
+++ src/muse_core.h
@@ -1,1 +1,32 @@
-/* TODO include all muse_core headers + deps */
+/* Copyright 2014 InteraXon, Inc. */
+
+/*
+ * Muse Core library, batteries included.
+ *
+ * This is the only file you need to include to build an application against
+ * Muse Core.
+ */
+
+#ifndef IX_MUSE_CORE_H_
+#define IX_MUSE_CORE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "defs.h"
+#include "result.h"
+#include "packet.h"
+#include "connect.h"
+#include "serial.h"
+#include "version.h"
+#include "util.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* IX_MUSE_CORE_H_ */
