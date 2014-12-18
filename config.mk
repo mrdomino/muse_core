fbd0b2d109edb5cd4fe4a529e49dfb6340d25f94
Steven <steve@smartercode.net>
2014-12-17 19:45:54 -0800

    XXX WIP config for windows build

--- config.mk
+++ config.mk
@@ -1,16 +1,10 @@
-CC = gcc
-LD = $(CC)
-CXX = clang++
+CC = cl
+LD = link
+CXX = $(CC)
 #CXX = g++
-CXXLD = $(CXX)
-CXXLDFLAGS += -fsanitize=address -fsanitize=undefined
+CXXLD = $(LD)
 
-PREFIX = /usr/local
-
-OFLAGS = -O0 -g
-WFLAGS = -Wall -Wextra -Werror -pedantic
-CFLAGS += $(OFLAGS) $(WFLAGS) -std=c11
-CXXFLAGS += $(OFLAGS) $(WFLAGS) -std=c++1y
+PREFIX =
 
 # Uncomment this to use bundled 3rdparty/hammer (not recommended)
 #USE_BUNDLED_HAMMER = yes
